3cc9ddddc5d86c9bc7941d7d2539c1fb554e4f7b
skp <samanza.kp@gmail.com>
2018-09-18 01:06:55 +0800

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LibraryComponent } from './library/library.component';
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+      component: HomeComponent,
+  },
+  {
+    path: 'about',
+      component: AboutComponent
+  },
+  {
+    path: 'library',
+    component: LibraryComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SimulationComponent } from './simulation/simulation.component';
+import { LibraryComponent } from './library/library.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SimulationComponent,
+    LibraryComponent,
+    FaqComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
